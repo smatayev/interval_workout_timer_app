dc0f4491b14c0776a5bc11e396226b8a202d37b0
shakhizat-matayev <153162992+shakhizat-matayev@users.noreply.github.com>
2024-11-26 08:03:55 +0000

    Added simple app tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Interval Timer heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Interval Timer/i);
+  expect(headingElement).toBeInTheDocument();
 });
+
+test('renders Settings component', () => {
+  render(<App />);
+  const settingsElement = screen.getByText(/Settings/i);
+  expect(settingsElement).toBeInTheDocument();
+});
+
+test('starts countdown on start button click', () => {
+  render(<App />);
+  const startButton = screen.getByText(/Start/i);
+  fireEvent.click(startButton);
+  const countdownElement = screen.getByText(/Starting in/i);
+  expect(countdownElement).toBeInTheDocument();
+});
+
+// test('pauses timer on pause button click', () => {
+//   render(<App />);
+//   const startButton = screen.getByText(/Start/i);
+//   fireEvent.click(startButton);
+//   const pauseButton = screen.getByText(/Pause/i);
+//   fireEvent.click(pauseButton);
+//   const pauseText = screen.getByText(/Paused/i);
+//   expect(pauseText).toBeInTheDocument();
+// });
