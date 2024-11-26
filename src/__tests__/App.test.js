3bb8ea6f89c3ff5ad347cf91bbef5c8d3e29f41d
shakhizat-matayev <153162992+shakhizat-matayev@users.noreply.github.com>
2024-11-26 08:28:21 +0000

    Added simple app tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders Interval Timer heading', () => {
   render(<App />);
